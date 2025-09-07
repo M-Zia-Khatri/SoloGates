89aa8fdb2a57280f944f8082d27b460f9c461c69
M-Zia-Khatri <kb313892@gmail.com>
2025-09-07 17:37:18 +0500

    add Owner Message sec and make the structure

--- client/src/pages/user_pages/AboutUs.tsx
+++ client/src/pages/user_pages/AboutUs.tsx
@@ -1,11 +1,15 @@
 import AboutUsBanner from "@/components/about/AboutUsBanner";
 import AboutUsSec from "@/components/about/AboutUsSec";
+import MissionVisionSec from "@/components/about/MissionVisionSec";
+import OwnerMessagesSec from "@/components/about/OwnerMessagesSec";
 
 export default function AboutUs() {
   return (
     <>
       <AboutUsBanner />
       <AboutUsSec />
+      <MissionVisionSec />
+      <OwnerMessagesSec />
     </>
   );
 }
