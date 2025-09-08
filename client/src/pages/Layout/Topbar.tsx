5a665d77a8dcc18146923973974d79adb280c069
M-Zia-Khatri <kb313892@gmail.com>
2025-09-08 21:26:20 +0500

    style the topbar and make it responsive

--- client/src/pages/Layout/Topbar.tsx
+++ client/src/pages/Layout/Topbar.tsx
@@ -1,38 +1,58 @@
 import { AppNavigation } from "@/constants/navigationConstants";
 import { assetsUrl } from "@/constants/urlConstants";
-import { useMemo, useState } from "react";
-import { href, Link } from "react-router-dom";
+import { useEffect, useMemo, useState } from "react";
+import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa6";
 
 export default function Topbar() {
   const [activePage, setActivePage] = useState<string>(AppNavigation.home);
 
+  useEffect(() => {
+    setActivePage(window.location.pathname);
+  }, [activePage]);
+
   const navLink = useMemo(
     () => [
-      { lable: "home", herf: AppNavigation.home },
-      { lable: "about", herf: AppNavigation.about },
-      { lable: "services", herf: AppNavigation.services },
+      { label: "home", href: AppNavigation.home },
+      { label: "about", href: AppNavigation.about },
+      { label: "services", href: AppNavigation.services },
     ],
     [],
   );
 
   return (
     <header className="sec-container">
-      <div className="flex h-[10vh] items-center justify-between">
-        <nav>
-          <ul className="flex w-full items-center space-x-1.5 capitalize">
+      <div className="border-Secondary text-md flex h-[8vh] items-center justify-between border-b-2 px-1 py-1.5 sm:px-2 md:px-3 lg:h-[12vh] lg:px-4 xl:px-5 2xl:text-lg">
+        {/* drawer bar  */}
+        <div className="md:hidden">
+          <FaBars size="24px" />
+        </div>
+
+        {/* Navigation link */}
+        <nav className="hidden md:block">
+          <ul className="flex w-full items-center space-x-3 capitalize lg:space-x-4 xl:space-x-5 2xl:space-x-6">
             {navLink.map((it, idx) => (
-              <Link to={it.herf} key={idx} onClick={() => setActivePage(href)}>
-                <li>{it.lable}</li>
+              <Link
+                to={it.href}
+                key={idx}
+                onClick={() => setActivePage(it.href)}
+                className={` ${it.href === activePage ? "activePage text-Secondary" : "underline-effect"}`}
+              >
+                <li>{it.label}</li>
               </Link>
             ))}
           </ul>
         </nav>
-        <div>
+
+        {/* contact button */}
+        <div className="btn-primary text-md px-2 py-1 font-semibold tracking-wide md:px-3 md:py-1.5 lg:px-4 lg:py-2 xl:px-5 xl:py-2.5 2xl:px-6 2xl:p-3">
           <Link className="capitalize" to={AppNavigation.contact}>
             Contact us
           </Link>
         </div>
-        <div className="h-full">
+
+        {/* logo */}
+        <div className="md:h[90%] h-[85%] max-h-[60px] min-h-[35px]">
           <img
             src={`${assetsUrl.logosUrl}LongLogoBlackBG.png`}
             className="h-full overflow-hidden object-cover object-center"
