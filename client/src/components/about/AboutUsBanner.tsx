cea6c019d4ddedc60a781fef336ca194e202ab62
M-Zia-Khatri <kb313892@gmail.com>
2025-09-10 02:30:42 +0500

    .

--- client/src/components/about/AboutUsBanner.tsx
+++ client/src/components/about/AboutUsBanner.tsx
@@ -20,13 +20,12 @@ const AboutUsBanner = () => {
       <div className="absolute top-0 -z-30 h-full w-full overflow-hidden">
         <div className="from-Secondary absolute bottom-1/2 left-1/2 h-[calc(80vw*0.8)] w-[80%] shrink-0 bg-radial to-[#00000000] to-80% opacity-40 blur-[10vw] md:to-65% lg:blur-[10vh]" />
         <div className="from-Secondary absolute top-1/2 right-1/2 h-[80vw] w-full shrink-0 bg-radial to-[#00000000] to-80% opacity-70 blur-[10vw] md:to-65% lg:blur-[10vh]" />
-
         <img
-          className={`absolute -top-[2.5%] -left-[12.5%] lg:-top-[10%] lg:-left-[25%] ${window.innerWidth < 1024 ? "w-full" : "h-full"} border border-red-500`}
+          className={`absolute -top-[2.5%] -left-[12.5%] lg:-top-[10%] lg:-left-[25%] ${window.innerWidth < 1024 ? "w-full" : "h-full"} `}
           src={`${assetsUrl.iconsUrl}sharpe1.svg`}
         />
         <img
-          className={`absolute -right-[12.5%] -bottom-[2.5%] lg:-right-[25%] lg:-bottom-[10%] ${window.innerWidth < 1024 ? "w-full" : "h-full"} shrink-0 rotate-z-180 border border-red-500`}
+          className={`absolute -right-[12.5%] -bottom-[2.5%] lg:-right-[25%] lg:-bottom-[10%] ${window.innerWidth < 1024 ? "w-full" : "h-full"} shrink-0 rotate-z-180`}
           src={`${assetsUrl.iconsUrl}sharpe1.svg`}
         />
       </div>
