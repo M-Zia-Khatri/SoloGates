469fbcabfa6c57455151b3b56c0442beec8f6421
M-Zia-Khatri <kb313892@gmail.com>
2025-09-09 16:16:18 +0500

    style the about us banner section and make it responsive

--- client/src/components/about/AboutUsBanner.tsx
+++ client/src/components/about/AboutUsBanner.tsx
@@ -2,14 +2,38 @@ import { assetsUrl } from "@/constants/urlConstants";
 
 const AboutUsBanner = () => {
   return (
-    <section className="sec-container flex h-[90vh] items-center justify-center">
+    <section
+      className="sec-container flex h-[90vh] items-center justify-center"
+      style={{}}
+    >
+      {/* banner image */}
       <img
-        className="absolute top-0 -z-50 h-screen w-full opacity-20"
+        className="absolute top-0 -z-50 h-full w-full opacity-20"
         src={`${assetsUrl.imagesUrl}banner.jpg`}
       />
-      <div className="bg-Bg-Primary/20 absolute top-0 -z-40 h-screen w-full" />
-      <div>
-        <img src={`${assetsUrl.imagesUrl}AboutUsText.png`} />
+
+      {/* background */}
+      <div className="bg-Secondary/5 absolute top-0 -z-40 h-full w-full" />
+
+      {/* decoration */}
+
+      <div className="absolute top-0 -z-30 h-full w-full overflow-hidden">
+        <div className="from-Secondary absolute bottom-1/2 left-1/2 h-[calc(80vw*0.8)] w-[80%] shrink-0 bg-radial to-[#00000000] to-80% opacity-40 blur-[10vw] md:to-65% lg:blur-[10vh]" />
+        <div className="from-Secondary absolute top-1/2 right-1/2 h-[80vw] w-full shrink-0 bg-radial to-[#00000000] to-80% opacity-70 blur-[10vw] md:to-65% lg:blur-[10vh]" />
+
+        <img
+          className={`absolute -top-[2.5%] -left-[12.5%] lg:-top-[10%] lg:-left-[25%] ${window.innerWidth < 1024 ? "w-full" : "h-full"} border border-red-500`}
+          src={`${assetsUrl.iconsUrl}sharpe1.svg`}
+        />
+        <img
+          className={`absolute -right-[12.5%] -bottom-[2.5%] lg:-right-[25%] lg:-bottom-[10%] ${window.innerWidth < 1024 ? "w-full" : "h-full"} shrink-0 rotate-z-180 border border-red-500`}
+          src={`${assetsUrl.iconsUrl}sharpe1.svg`}
+        />
+      </div>
+
+      {/* text */}
+      <div className="w-[85%] md:w-[75%] lg:w-[65%] 2xl:w-[60%]">
+        <img className="w-full" src={`${assetsUrl.imagesUrl}AboutUsText.png`} />
       </div>
     </section>
   );
