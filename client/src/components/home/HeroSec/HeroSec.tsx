9b361a58c16c5a14acbdb6ceb23ba77566bf7e7b
M-Zia-Khatri <kb313892@gmail.com>
2025-09-24 23:53:17 +0500

    Enhance responsiveness of HeroSec component; adjust height and positioning for various screen sizes

--- client/src/components/home/HeroSec/HeroSec.tsx
+++ client/src/components/home/HeroSec/HeroSec.tsx
@@ -5,9 +5,9 @@ import Glow from '@/components/ui/Glow';
 const HeroSec = () => {
   return (
     <section className="sec-container overflow-x-hidden">
-      <div className="h-[94vh] lg:h-[90vh]">
+      <div className="h-[94vh] md:h-[84vh] lg:h-[90vh]">
         {/* background */}
-        <div className="absolute top-0 left-0 -z-50 h-screen w-full overflow-hidden opacity-75">
+        <div className="absolute top-0 left-0 -z-50 h-screen w-full overflow-hidden opacity-75 md:h-[90vh] lg:h-screen">
           <img
             src={`${assetsUrl.imagesUrl}dottedBG.png`}
             alt="background image"
@@ -16,44 +16,47 @@ const HeroSec = () => {
         </div>
 
         {/* decoration */}
-        <div className="absolute top-0 left-0 -z-40 h-full w-full overflow-x-hidden">
+        <div className="absolute top-0 left-0 -z-40 h-full w-full overflow-x-hidden md:h-[90vh] lg:h-screen">
           <Glow className="top-[5%] -left-[15%] z-[-55] opacity-85" />
           <img
-            className="absolute top-[25%] -left-[8%] h-16 rotate-z-[-50deg]"
+            className="absolute top-[25%] -left-[8%] h-16 rotate-z-[-50deg] md:top-[30%] md:-left-[5%] md:h-20 lg:top-[35%] lg:-left-[2%]"
             src={`${assetsUrl.imagesUrl}triangle-shape.png`}
           />
           <Glow className="-right-[15%] bottom-[5%] z-[-55] opacity-85" />
           <img
-            className="absolute -right-[5%] bottom-[25%] h-16 rotate-z-[120deg]"
+            className="absolute -right-[5%] bottom-[25%] h-16 rotate-z-[120deg] md:-right-[5%] md:bottom-[30%] md:h-20 lg:-right-[2%] lg:bottom-[35%]"
             src={`${assetsUrl.imagesUrl}triangle-shape.png`}
           />
         </div>
 
-        <div className="flex h-full flex-col justify-between">
+        <div className="flex h-full flex-col justify-between overflow-hidden">
           {/* text */}
-          <div className="mt-4 h-fit px-0 md:relative md:-z-40 md:px-8">
+          <div className="mt-4 h-fit px-0 md:mt-6 lg:relative lg:-z-40 lg:px-8">
             <img src={`${assetsUrl.imagesUrl}HomeText.png`} alt="" />
-            <div className="absolute bottom-0 left-0 z-40 w-full bg-gradient-to-t from-black from-10% to-transparent to-[110%] py-4 text-center md:-bottom-6 md:left-8 md:w-1/3 md:bg-none">
-              <p className="font-Sora text-[10px]" lang="de">
-                From brand identity to high-impact ads, Sologate{' '}
-                <br className="hidden md:block" />
-                Transforms your ideas into campaigns that don’t{' '}
-                <br className="hidden md:block" /> just Look good - they drive
+            <div className="absolute bottom-0 left-0 z-40 flex h-1/2 w-full items-end bg-gradient-to-t from-black from-20% to-transparent to-[100%] text-center md:bottom-[calc(10vh-2.5px)] md:h-1/3 lg:-bottom-4 lg:left-9 lg:h-fit lg:w-[30%] 2xl:w-[40%] lg:bg-none lg:text-left xl:-bottom-2 xl:left-12 2xl:-bottom-1 2xl:left-16">
+              <p
+                className="font-Sora px-8 py-4 text-[10px] md:px-[20%] md:py-6 lg:px-0 lg:py-0 2xl:text-lg"
+                lang="de"
+              >
+                From brand identity to high-impact ads, Sologate
+                <br className="hidden lg:block" />
+                Transforms your ideas into campaigns that don’t
+                <br className="lg :block hidden" /> just Look good - they drive
                 results.
               </p>
             </div>
           </div>
           {/* img */}
-          <div className="top-0 z-10 h-full overflow-hidden md:absolute md:h-screen">
+          <div className="top-0 z-10 h-full scale-[1.2] md:scale-[1] lg:absolute lg:left-1/2 lg:h-screen lg:-translate-x-1/2 lg:scale-[0.9] lg:overflow-hidden">
             <img
-              className="relative -z-40 h-full md:top-4"
+              className="relative -z-40 h-full w-fit md:-top-[12%] lg:top-4"
               src={`${assetsUrl.imagesUrl}heroImage.png`}
             />
           </div>
         </div>
       </div>
       {/* hook */}
-      <div className="bottom-0 left-0 z-10 my-4 grid w-full grid-cols-2 gap-3 from-black from-20% to-transparent to-30% md:absolute md:flex md:gap-7 md:bg-gradient-to-t md:px-16 md:pb-8">
+      <div className="bottom-0 left-0 z-10 my-4 grid w-full grid-cols-2 gap-3 from-black from-20% to-transparent to-[102%] md:my-6 md:flex lg:absolute lg:my-0 lg:gap-7 lg:bg-gradient-to-t lg:px-8 lg:pt-[10%] lg:pb-10">
         <HeroCard
           heading="Branding:"
           subtitle={['Your visual story', 'made unforgettable.']}
