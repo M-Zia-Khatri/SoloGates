484ca19e74c09f5cb9f28756435e16242617fd04
M-Zia-Khatri <kb313892@gmail.com>
2025-09-25 00:59:49 +0500

    feat: add ServicesBannerSec  with background, decoration elements and fully responsive. add TrustedBy section into server with heading

--- client/src/pages/user_pages/Services.tsx
+++ client/src/pages/user_pages/Services.tsx
@@ -1,6 +1,56 @@
+import ServicesBannerSec from '@/components/services/ServicesBannerSec';
+import TrustedBy from '@/components/TrustedBy';
+import HrLine from '@/components/ui/HrLine';
+import { assetsUrl } from '@/constants/urlConstants';
+
+const items = [
+  {
+    src: `${assetsUrl.imagesUrl}company/company1.png`,
+    alt: 'company1',
+  },
+  {
+    src: `${assetsUrl.imagesUrl}company/company2.png`,
+    alt: 'company2',
+  },
+  {
+    src: `${assetsUrl.imagesUrl}company/company3.png`,
+    alt: 'company3',
+  },
+  {
+    src: `${assetsUrl.imagesUrl}company/company4.png`,
+    alt: 'company4',
+  },
+  {
+    src: `${assetsUrl.imagesUrl}company/company5.png`,
+    alt: 'company5',
+  },
+  {
+    src: `${assetsUrl.imagesUrl}company/company6.png`,
+    alt: 'company6',
+  },
+];
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <>
+      <ServicesBannerSec />
+      <section className="sec-container">
+        <div className="mt-6 mb-2 space-y-4 md:mt-7 md:mb-3 md:space-y-5 lg:mt-8 lg:mb-4 lg:space-y-6 xl:mt-9 xl:mb-5 xl:space-y-7 2xl:mt-10 2xl:mb-6 2xl:space-y-8">
+          {/* heading & underline */}
+          <div className="mt-2 flex items-center justify-center gap-1.5 lg:mt-4">
+            <HrLine className="h-[3px] w-[4vw]" />
+            <h3 className="flex shrink-0 font-semibold">
+              Trusted by&nbsp;
+              <h3 className="font-thin italic">Visionaries</h3>
+            </h3>
+            <HrLine className="h-[3px] w-[4vw]" isRotate />
+          </div>
+          {/* content */}
+          <TrustedBy speed={50} items={items} gap={45} />
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Services;
